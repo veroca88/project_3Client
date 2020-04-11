01c0e05a62ca6977f61966055f64c15758001be0
Veronica Donoso <veronicadonoso1506@gmail.com>
2020-04-11 14:56:40 -0400

    Add accordion images, do some style home page and products page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,76 @@
 import React from "react";
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route } from "react-router-dom";
 
 //Components
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import Signup from "./components/Authentication/Signup";
 import Login from "./components/Authentication/Login";
-import Profile from "./components/User/Profile"
+// import Profile from "./components/User/Profile";
+import DisplayProducts from "./components/Products/DisplayProducts";
 
 //Styles
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <Navbar />
-      </header>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/signup' component={Signup} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/profile' component={Profile} />
-        {/* <Route exact path='/private' component={Private} /> */}
-      </Switch>
-      <footer>
-        Made with
-        <span role='img' aria-label='emoji'>
-          ♥️
-        </span>
-        at Ironhack Miami - PTWD October 2019
-      </footer>
-    </div>
-  );
+import axios from "axios";
+
+class App extends React.Component {
+  state = {
+    postProducts: null,
+  };
+
+  componentDidMount () {
+    this.getProducts();
+  };
+
+  getProducts = () => {
+    axios
+      .get('http://localhost:3001/products')
+      .then((productsFromAPI) => {
+        this.setState({
+          postProducts: productsFromAPI.data,
+        });
+        console.log('getting all my products ==================================', productsFromAPI.data[0] )
+      })
+      .catch((err) => console.log({ err }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <Navbar />
+        </header>
+        <Switch>
+        <Route
+            exact
+            path="/products"
+            render={(props) => (
+              <DisplayProducts
+              {...props}
+                getProducts={this.getProducts}
+                allProducts={this.state.postProducts}
+              />
+            )}
+          />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path='/private' component={Private} /> */}
+          {/* <Route exact path='/details' render={(props) => (<DisplayProducts {...props} getProducts={this.getProducts} />
+                      )} />*/}
+        </Switch>
+        <footer>
+          Made with
+          <span role="img" aria-label="emoji">
+            ♥️
+          </span>
+          at Ironhack Miami - PTWD October 2019
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,18 @@
-
 import React from 'react';
+
 const Home = () => {
   return (
-    <>
-      <h1>This is the home page!</h1>
-    </>
+    <div class="container">
+  <h1>Best Sellers</h1>
+
+  <div class="gallery-wrap">
+    <div class="item item-1"></div>
+    <div class="item item-2"></div>
+    <div class="item item-3"></div>
+    <div class="item item-4"></div>
+    <div class="item item-5"></div>
+  </div>
+ </div>
   );
 };
 
