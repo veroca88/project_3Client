abdbf7f16c6eb0aca2816160766f7845c0526374
Veronica Donoso <veronicadonoso1506@gmail.com>
2020-04-20 22:35:02 -0400

    Delete unuseless files

--- src/components/DescriptionOfProduct/DescriptionProduct.js
+++
@@ -1,8 +0,0 @@
-{/* option2 */}
-                                                                                              {/* <div class="tab pink">
-                                                                                                <Button variant="danger">
-                                                                                                  <i class="far fa-heart"></i>
-                                                                                                </Button>
-                                                                                              </div> */}
-                                                                                              {/* <p>material: {product.material}</p>
-                                                                                                            <a href="#">In Stock: {product.designer ? "Yes" : "No"}</a> */}
