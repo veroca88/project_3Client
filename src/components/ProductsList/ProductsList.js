91020aacb3eee2c43607c8ee663feca9f42360a0
Veronica Donoso <veronicadonoso1506@gmail.com>
2020-04-29 23:18:24 -0400

    Merge two page in one, keep it simple, Search bar works

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -2,23 +2,122 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import axios from "axios";
+import Search from "../Search/Search";
+import { AuthContext } from "../context/Authentication";
+
 class ProductList extends React.Component {
+  state = {
+    postProducts: null,
+    search: []
+  };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts = () => {
+    axios
+      .get("http://localhost:3001/products")
+      .then((productsFromAPI) => {
+        this.setState({
+          postProducts: productsFromAPI.data,
+          search: productsFromAPI.data,
+        });
+        console.log("products", this.state.postProducts);
+      })
+      .catch((err) => console.log({ err }));
+  };
+
+  // handle search by description of product (is checked it is ok!!)
+
+  handleSearchItems = (e) => {
+    const { value } = e.target;
+    const { postProducts } = this.state;
+
+    const searchItems = postProducts.filter((item) => {
+      console.log("Here is the iteeeeeeeeeeeeeem", item);
+      return item.description.toUpperCase().includes(value.toUpperCase());
+      // for (let ea in item) {
+      //   if (item[ea] === value) {
+      //     return item[ea].toLowerCase().includes(value.toLowerCase())
+      //   }
+      // }
+    });
+    this.setState({
+      search: searchItems,
+      postProducts: postProducts,
+    });
+  };
+
+  // // handle each option in product description
+
+  // handleItem = (e) => {
+  //   // e.preventDefault()
+  //   const { value, name } = e.target;
+  //   console.log("this is value", value);
+  //   console.log("this is name", name);
+  //   // const { product } = this.state;
+  //   // console.log("COLOR", value);
+  //   this.setState((prevState) => ({
+  //     // ...prevState,
+  //     orderItem: {
+  //       ...prevState.orderItem,
+  //       [name]: value,
+  //       inShopBag: true,
+  //     },
+  //   }));
+  //   console.log("this is our order", this.state.orderItem);
+  // };
+
+  // addToCart = () => {};
+
   render() {
-    const { name, cost, image, _id } = this.props;
     return (
-        <div className="cardview">
-        <div  className="content">
-        {/* <Route exact path="/details" render={(props) => (<OneProduct {...props}/>)} /> */}
-          <Link to={`/products/${_id}`}>
-            <img src={image} alt={name}/>
-          </Link>
-            <p >{name}</p>
-            <p >${cost}</p>
-          <div>
-              <i className="far fa-heart heart-click"></i>
-          </div>                                                                    
-        </div>
-      </div>
+      <AuthContext.Consumer>
+        {(context) => {
+          const { currentUser } = context.state;
+
+          const { search, postProducts, orderItem } = this.state;
+
+          return (
+            <>
+              <div>
+                <Search handleSearchItems={this.handleSearchItems} />
+                <div className="box">
+                  {search ? (
+                    search.map((eachProduct, index) => (
+                      <div key={index} className="cardview">
+                        <div className="content">
+                          <Link
+                            to={{
+                              pathname: `/products/${eachProduct._id}`,
+                              state: {
+                                productDetail: eachProduct,
+                                currentUser: currentUser
+                              },
+                            }}
+                          >
+                            <img
+                              className="card-img"
+                              src={eachProduct.image}
+                              alt={eachProduct.name}
+                            />
+                          </Link>
+                          <p>{eachProduct.name}</p>
+                          <p>${eachProduct.cost}</p>
+                        </div>
+                      </div>
+                    ))
+                  ) : (
+                    <h1> L O A D I N G . . . </h1>
+                  )}
+                </div>
+              </div>
+            </>
+          );
+        }}
+      </AuthContext.Consumer>
     );
   }
 }
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,17 @@
-import React from "react";
 
-export default function Search({handleSearch}) {
+import React from 'react'
+
+export default function Search({handleSearchItems}) {
   return (
-    <div>
-      <input
-      onChange={handleSearch}
-        id="search"
-        name="search"
-        placeholder="Search Products"
-        style={{ width: "50%" }}
-      />
-    </div>
-  );
+      <div>
+        <input
+          onChange={handleSearchItems}
+          id="search"
+          name="search"
+          placeholder="Search Products"
+          style={{ width: "50%" }}
+        />
+      </div>
+  )
 }
+
