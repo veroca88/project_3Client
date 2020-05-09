d24a634db433bcf345578054eeca8a775baad30c
Veronica Donoso <veronicadonoso1506@gmail.com>
2020-05-09 01:01:28 -0400

    Daily Inspiration Page

---
+++ src/pages/DailyInspirational.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import RandomProduct from "../components/RandomProduct.js/RandomProduct"
+
+export default function DailyInspirational() {
+    return (
+        <>
+            <RandomProduct/>
+        </>
+    )
+}
+
