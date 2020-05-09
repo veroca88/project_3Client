4b8b68ebf07e188360ef527bf0e2fe46c4bdeed0
Veronica Donoso <veronicadonoso1506@gmail.com>
2020-05-09 08:14:48 -0400

    Create a link to products

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from "react-router-dom"
 const Home = () => {
   return (
     <div className="container">
   {/* <h1>Best Sellers</h1> */}
-
+<Link to="/products">
   <div className="gallery-wrap">
     <div className="item item-1"></div>
     <div className="item item-2"></div>
@@ -11,6 +12,7 @@ const Home = () => {
     <div className="item item-4"></div>
     <div className="item item-5"></div>
   </div>
+</Link>
  </div>
   );
 };
